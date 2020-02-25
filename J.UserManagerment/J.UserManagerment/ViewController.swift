5f50e41da8e69304748b5d4ee7a0dfc72cf830bb
JinYoung Lee <kulky@anipen.com>
2020-02-25 10:50:54 +0900

    init project

---
+++ J.UserManagerment/J.UserManagerment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  J.UserManagerment
+//
+//  Created by JinYoung Lee on 2020/02/25.
+//  Copyright © 2020 JinYoung Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
